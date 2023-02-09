b524f14ea12a7a717f282d8de83d34d4d820e9f1
William Luo <luozhange@gmail.com>
2023-02-09 15:57:52 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    
+
+    public static void main(String[] args) {
+	// write your code here
+        String msg = "hello world!";
+        System.out.printf("%s",msg);
+    }
+}
